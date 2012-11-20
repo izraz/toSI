b4620b0ffb4f864ff1f63634a4039059088e6c10
Bojan Durickovic <bojan@bojand.org>
2012-11-20 13:51:50 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Units conversion to SI
+
+This package is for convenience only. It merely defines variables for quick units conversion.
+
+# Example
+
+	from toSI import MeV
+	
+	energy = 931 * MeV #  931 MeV with a numeric value in Joules
+	
---
+++ __init__.py
@@ -0,0 +1,18 @@
+"""Some alternative units expressed in their SI counterparts."""
+
+# length (in meters)
+inch = 0.0254 # [m] inch  ('in' is a reserved word in Python)
+mm = 0.001 # [m]
+cm = 0.01 # [m]
+km = 1e3 # [m]
+
+# mass (in kilograms)
+amu = 1.66053886e-27 # [kg] atomic mass unit 
+
+# energy (in Joules)
+eV = 1.60217646e-19 # [J] electron-Volt
+keV = 1.60217646e-16 # [J] kilo electron-Volt
+MeV = 1.60217646e-13 # [J] mega electron-Volt
+
+# pressure (in Pascals)
+Torr = 101325. / 760. # [Pa] Torr (approx. 133.3224 Pa)
