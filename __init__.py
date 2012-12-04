714b7120f7266b028b340a6fa7137f4733e04815
Bojan Durickovic <bojan@bojand.org>
2012-12-04 11:55:38 -0500

    Add bar, millibar

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 """Some alternative units expressed in their SI counterparts.
 
+This only provides scaling factors.
+The idea is to scale manually with no units overhead. 
+
 This is for convenience only. For a more complete standard package,
 consider e.g. sympy.physics.units 
 """
@@ -9,9 +12,9 @@ ms = 1e-3 # [s] millisecond
 us = 1e-6 # [s] microsecond
 ns = 1e-9 # [s] nanosecond
 ps = 1e-12 # [s] picosecond
-minute = 60 # [s] minute ('min' is for minimum)
-h = hr = hour = 3600 # [s] hour
-day = 86400 # [s] day
+minute = 60. # [s] minute ('min' is for minimum)
+h = hr = hour = 3600. # [s] hour
+day = 86400. # [s] day
 week = 7 * day # [s] week
 yr = year = 365.25 * day # [s] year
 mo = month = yr / 12. # [s] average month
@@ -27,10 +30,13 @@ km = 1e3 # [m] kilometer
 amu = 1.66053886e-27 # [kg] atomic mass unit 
 lb = pound = 0.45359237 # [kg] pound
 
-# energy (in Joules)
-eV = 1.60217646e-19 # [J] electron-Volt
-keV = 1.60217646e-16 # [J] kilo electron-Volt
-MeV = 1.60217646e-13 # [J] mega electron-Volt
+# energy (in joules)
+eV = 1.60217646e-19 # [J] electron volt
+keV = 1.60217646e-16 # [J] kilo-electron volt
+MeV = 1.60217646e-13 # [J] mega-electron volt
+GeV = 1.60217646e-10 # [J] giga-electron volt
 
-# pressure (in Pascals)
+# pressure (in pascals)
 Torr = 101325. / 760. # [Pa] Torr (approx. 133.3224 Pa)
+bar = 1e5 # [Pa] bar
+mbar = 100. # [Pa] millibar
